a1b08a9d7b599863eb3f7bb9d1d6a44a901b0aef
Fun741 <89957391+Fun741@users.noreply.github.com>
2022-03-21 20:02:01 -0500

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -2,14 +2,21 @@
     class Database {
         //DB params
         
-        
+        /*
         private $host = 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         private $db_name ='qq98270q4ma5o9jx';
         private $username = 't468dm1c55876o10';
         private $password = 'thn3v0224bqd1ybr';
         private $conn;
+        */
         
- 
+        $url = getenv('JAWS_DB');
+        $dbparts = parse_url($url);
+        
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
         
         /*
         $url = getenv('JAWS_DB');
